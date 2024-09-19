383bcbc8498647b8e6f4ba6dcca8120c31d0b8f8
Siddharth Gowda <siddharth@skube.xyz>
2024-09-19 17:19:49 -0400

    simulation complete

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,51 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates Number of Marriages Per Month in Toronto
+# Author: Siddharth Gowda
+# Date: 2024-Sep-19
+# Contact: siddharth.gowda@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? All data will be from 2000 and beyond
 
 
 #### Workspace setup ####
 library(tidyverse)
 # [...UPDATE THIS...]
+set.seed(888)
 
 #### Simulate data ####
 # [...ADD CODE HERE...]
+
+lambda <- 10 # average marriages per month
+sample_size <- 100
+
+marriage_data <- tibble(
+  year = rep(2000:2023, each = 12),
+  month_num = rep(1:12, times = 24),
+  marriages = rpois(n = 24 * 12, lambda = 10)  # Using Poisson distribution for number of marriages
+)
+
+marriage_data <- marriage_data %>% group_by(month_num) %>% 
+  summarise(marriages = sum(marriages))
+
+marriage_data <- marriage_data %>%
+  mutate(month = case_when(
+    month_num == 1 ~ "SEP",
+    month_num == 2 ~ "OCT",
+    month_num == 3 ~ "NOV",
+    month_num == 4 ~ "DEC",
+    month_num == 5 ~ "JAN",
+    month_num == 6 ~ "FEB",
+    month_num == 7 ~ "MAR",
+    month_num == 8 ~ "APR",
+    month_num == 9 ~ "MAY",
+    month_num == 10 ~ "JUN",
+    month_num == 11 ~ "JUL",
+    month_num == 12 ~ "AUG",
+    TRUE ~ NA_character_  # Default for any unmatched value
+  )) %>% select(month, marriages)
+# first intermediate table
+# year, month, number of marriages
 
 
 
